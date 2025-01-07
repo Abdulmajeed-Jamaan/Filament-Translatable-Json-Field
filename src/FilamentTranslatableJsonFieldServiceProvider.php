f70f1475e5a53fcc6e5cd5c249a8389924adf9d3
Abdulmajeed-Jamaan <Abdulmajeed-Jamaan@users.noreply.github.com>
2025-01-07 00:00:15 +0000

    Fix styling

--- src/FilamentTranslatableJsonFieldServiceProvider.php
+++ src/FilamentTranslatableJsonFieldServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace AbdulmajeedJamaan\FilamentTranslatableJsonField;
 
+use AbdulmajeedJamaan\FilamentTranslatableJsonField\Commands\FilamentTranslatableJsonFieldCommand;
+use AbdulmajeedJamaan\FilamentTranslatableJsonField\Testing\TestsFilamentTranslatableJsonField;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use AbdulmajeedJamaan\FilamentTranslatableJsonField\Commands\FilamentTranslatableJsonFieldCommand;
-use AbdulmajeedJamaan\FilamentTranslatableJsonField\Testing\TestsFilamentTranslatableJsonField;
 
 class FilamentTranslatableJsonFieldServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace AbdulmajeedJamaan\FilamentTranslatableJsonField\Tests;
 
+use AbdulmajeedJamaan\FilamentTranslatableJsonField\FilamentTranslatableJsonFieldServiceProvider;
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
@@ -16,7 +17,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use AbdulmajeedJamaan\FilamentTranslatableJsonField\FilamentTranslatableJsonFieldServiceProvider;
 
 class TestCase extends Orchestra
 {
